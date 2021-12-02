9653cf863a612852d539f66205b805ce095b9eb2
TidalSurf <64665142+TidalSurf@users.noreply.github.com>
2021-12-02 12:26:12 -0600

    setup

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,47 +1,50 @@
-import { Router, Response, Request } from "express";
+import { Response, Request } from "express";
 import { DB } from "../db/db";
 export class Routes {
-	public router: Router;
-	public db: DB;
-	constructor() {
-		this.router = Router();
-		this.db = new DB();
-	}
+  public db: DB;
+  constructor() {
+    this.db = new DB();
+  }
 
-	/**
-	 * Fetch all data from route "/:address"
-	 * @return {Response} Response when fetching specific data using url query.
-	 */
-	public index = async (req: Request, res: Response) => {
-		await this.db.insert("test", [
-			{
-				test: "test",
-			},
-		]);
-		res.send({ success: true });
-	};
+  /**
+   * Fetch all data from route "/:address"
+   * @return {Response} Response when fetching specific data using url query.
+   */
+  public index = async (req: Request, res: Response) => {
+    await this.db.insert("test", 3333, [
+      {
+        test: "test",
+      },
+    ]);
+    res.send({ success: true });
+  };
 
-	/**
-	 * Scrape data with post request to address route "/contract"
-	 * @return {Response} Response when finished scraping.
-	 */
-	public deleteTable = async (req: Request, res: Response) => {
-		res.send({ success: true });
-	};
+  /**
+   * Scrape data with post request to address route "/contract"
+   * @return {Response} Response when finished scraping.
+   */
+  public scrape = async (req: Request, res: Response) => {};
 
-	public createTable = async (req: Request, res: Response) => {
-		res.send({ success: true });
-	};
+  public deleteTable = async (req: Request, res: Response) => {
+    await this.db.deleteTable();
+    res.send({ success: true });
+  };
 
-	public createDB = async (req: Request, res: Response) => {
-		res.send({ success: true });
-	};
+  public createTable = async (req: Request, res: Response) => {
+    await this.db.createTable();
+    res.send({ success: true });
+  };
 
-	public deleteDB = async (req: Request, res: Response) => {
-		res.send({ success: true });
-	};
+  public createDB = async (req: Request, res: Response) => {
+    res.send({ success: true });
+  };
 
-	public deleteData = async (req: Request, res: Response) => {
-		res.send({ success: true });
-	};
+  public deleteDB = async (req: Request, res: Response) => {
+    res.send({ success: true });
+  };
+
+  public deleteData = async (req: Request, res: Response) => {
+    await this.db.deleteData();
+    res.send({ success: true });
+  };
 }
