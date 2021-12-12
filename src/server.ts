bb21e8dd5d67f3cb07fe906fd1202aabf8a54035
TidalSurf <64665142+TidalSurf@users.noreply.github.com>
2021-12-12 14:01:46 -0600

    fixed ranking for production

--- src/server.ts
+++ src/server.ts
@@ -6,63 +6,67 @@ import bodyParser from "body-parser";
 import config from "../config.json";
 const rateLimit = require("express-rate-limit");
 const createAccountLimiter = rateLimit({
-	windowMs: 30 * 60 * 1000, // 30 min window
-	max: 2, // start blocking after 2 requests
-	message: "Too many requests from this IP, please try again after 30 mins",
+  windowMs: 30 * 60 * 1000, // 30 min window
+  max: 2, // start blocking after 2 requests
+  message: "Too many requests from this IP, please try again after 30 mins",
 });
 const apiLimiter = rateLimit({
-	windowMs: 5 * 60 * 1000, // 5 minutes
-	max: 20,
-	message: "Too many requests from this IP, please try again after 5 mins",
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 20,
+  message: "Too many requests from this IP, please try again after 5 mins",
 });
 
 class Server {
-	private app: express.Application;
-	private port: number;
-	private router: Routes;
-	constructor() {
-		this.app = express();
-		this.port = config.PORT;
-		this.router = new Routes();
-		this.configuration();
-		this.routes();
-	}
+  private app: express.Application;
+  private port: number;
+  private router: Routes;
+  constructor() {
+    this.app = express();
+    this.port = config.PORT;
+    this.router = new Routes();
+    this.configuration();
+    this.routes();
+  }
 
-	/**
-	 * Express Server Configuration
-	 */
-	public configuration() {
-		this.app.use(bodyParser.urlencoded({ extended: false }));
-		this.app.use(bodyParser.json());
-		this.app.use(helmet());
-		this.app.use(cors());
-	}
+  /**
+   * Express Server Configuration
+   */
+  public configuration() {
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    this.app.use(bodyParser.json());
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
 
-	/**
-	 * Express Router Configuration
-	 */
-	public routes() {
-		this.app.get("/api/:contractaddress", apiLimiter, this.router.index);
-		this.app.post(
-			"/api/contractaddress",
-			createAccountLimiter,
-			this.router.scrape
-		);
-		this.app.post("/api/stats", apiLimiter, this.router.stats);
-		this.app.get("/deleteTable", this.router.deleteTable);
-		this.app.get("/deleteData", this.router.deleteData);
-		this.app.get("/createDB", this.router.createDB);
-		this.app.get("/deleteDB", this.router.deleteDB);
-	}
+  /**
+   * Express Router Configuration
+   */
+  public routes() {
+    this.app.get("/api/:contractaddress", apiLimiter, this.router.index);
+    this.app.post(
+      "/api/contractaddress",
+      createAccountLimiter,
+      this.router.scrape
+    );
+    this.app.post("/api/stats", apiLimiter, this.router.stats);
+    this.app.get("/deleteTable", this.router.deleteTable);
+    this.app.get(
+      "/api/deleteData",
+      createAccountLimiter,
+      this.router.deleteData
+    );
+    this.app.get("/createDB", this.router.createDB);
+    this.app.get("/deleteDB", this.router.deleteDB);
+  }
 
-	/**
-	 * Start Express Server
-	 */
-	public start() {
-		this.app.listen(this.port, () => {
-			console.info("Server is listening on", this.port);
-		});
-	}
+  /**
+   * Start Express Server
+   */
+  public start() {
+    this.app.listen(this.port, () => {
+      console.info("Server is listening on", this.port);
+    });
+  }
 }
 
 const server = new Server();
