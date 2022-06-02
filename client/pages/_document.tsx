f1ccdb5415f0e3c372db05962d1b9e4d8c5ca657
wavygoten <bossmanzach007@gmail.com>
2022-06-02 01:10:38 -0500

    added prod command

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -6,16 +6,6 @@ export default class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx)
     const critical = extractCritical(initialProps.html)
     initialProps.html = critical.html
-    // initialProps.styles = (
-    //   <React.Fragment>
-    //     {initialProps.styles}
-    //     <style
-    //       data-emotion-css={critical.ids.join(' ')}
-    //       dangerouslySetInnerHTML={{ __html: critical.css }}
-    //     />
-    //   </React.Fragment>
-    // )
-
     return initialProps
   }
 
