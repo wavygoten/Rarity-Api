3e6a4f8d955270ff28b67e1112fd6cbac7f8b060
Ihsaan Bijapuri <ihsaan@Ihsaans-MacBook-Pro.local>
2025-09-08 03:23:20 -0500

    removed connect to wallet

--- client/src/hooks/usePagination.tsx
+++ client/src/hooks/usePagination.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function usePagination(data: any, itemsPerPage: number) {
 	const [currentPage, setCurrentPage] = useState<number>(1);
